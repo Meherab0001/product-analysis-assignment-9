e3d5b559483bad4c54e4239eb57fc32babcca89a
Meherab211 <meherab395@gmail.com>
2022-04-04 23:16:02 +0600

    added home page

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Area, Bar, CartesianGrid, ComposedChart, Line, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+
 
 const Dashboard = () => {
+    const data = 'data.json'
     return (
         <div>
-            <h3>this daschborad from user </h3>
+            <div>
+                <ComposedChart width={730} height={250} data={data}>
+                    <XAxis dataKey={'sell'}></XAxis>
+                    <YAxis dataKey={'investment'}></YAxis>
+                    <Tooltip></Tooltip>
+                    <CartesianGrid stroke="#f5f5f5"></CartesianGrid>
+               
+                   
+                  
+                   
+                </ComposedChart>
+            </div>
+            <div>
+                <PieChart>
+                    <Pie dataKey={'sell'} nameKey={'investment'} cx="50%" cy="50%" outerRadius={50} fill="#8884d8"></Pie>
+                </PieChart>
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-           <h3>this home and main page </h3>
+        <div className='flex grid-cols-2 gap-8 mt-5 justify-between p-5'>
+           <div>
+               <img className='w-25 h-25' src="https://i.ytimg.com/vi/DsTYK3jSnM8/maxresdefault.jpg" alt="" />
+           </div>
+           <div>
+               <h2 className='text-5xl text-orange-500 font-serif bold '>This Goast BIKE</h2>
+               <h2 className='text-5xl text-blue-400 font-serif bold '>Chose your best bike here</h2>
+          <p> At GHOST we have been building bikes for over 25 years. It all started with two friends, a small garage and the idea to build the exact bikes that you would like to ride yourself. Each of our bikes has its own character that makes it special. From the good-natured beginner bike to the aggressive enduro bike, there are many nuances.</p>
+          <button className='bg-gray-500 mt-5 border p-3 text-white border-r-4'>Live Demo</button>
+           </div>
+         
         </div>
     );
 };
